4a05cfeb360271e03b2aee1d0838a8b3721e872c
Miguel Uranga <57147319+Lemonaeds@users.noreply.github.com>
2024-10-03 22:18:00 +0000

    Creacion de la excusa

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  document.getElementById("excuse").innerHTML =
+    who[Math.floor(Math.random() * 3 + 1)] +
+    " " +
+    action[Math.floor(Math.random() * 3 + 1)] +
+    " " +
+    what[Math.floor(Math.random() * 3 + 1)] +
+    " " +
+    when[Math.floor(Math.random() * 3 + 1)];
   console.log("Hello Rigo from the console!");
 };
